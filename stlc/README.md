4f2a993615b7d1c64197c32a3bb0b98cfab7f50e
Romain Ruetschi <romain.ruetschi@gmail.com>
2017-12-05 19:40:15 +0100

    Add partial port of the Dafny implementation

---
+++ stlc/README.md
@@ -0,0 +1,8 @@
+
+## Soundness of the simply typed lambda calculus
+
+The code is this folder is based on [Nada Amin](http://lampwww.epfl.ch/~amin/cv/)'s talk "[How to write your next POPL paper in Dafny](https://www.microsoft.com/en-us/research/video/how-to-write-your-next-popl-paper-in-dafny/)".
+
+- `Stlc.dfy` is a partial retranscription of the code presented in the talk.
+- `STLC.scala` is partial port of `Stlc.dfy` to *PureScala*.
+
---
+++ stlc/STLC.scala
@@ -0,0 +1,261 @@
+
+import stainless.lang._
+import stainless.annotation._
+import stainless.proof._
+
+object stlc {
+
+  sealed abstract class Ty
+  final case class TBool()                extends Ty
+  final case class TArrow(t1: Ty, t2: Ty) extends Ty
+
+  sealed abstract class Term {
+
+    def isValue: Boolean = this match {
+      case Abs(_, _, _)     => true
+      case True()           => true
+      case False()          => true
+      case _                => false
+    }
+
+    def fv: Set[BigInt] = this match {
+      case Var(id)           => Set(id)
+      case App(f, arg)       => f.fv ++ arg.fv
+      case Abs(x, _, body)   => body.fv -- Set(x)
+      case If(cnd, thn, els) => cnd.fv ++ thn.fv ++ els.fv
+      case True()            => Set()
+      case False()           => Set()
+    }
+
+    def isClosed: Boolean = forall { (x: BigInt) =>
+      !fv.contains(x)
+    }
+
+    def step: Option[Term] = this match {
+      case App(Abs(x, _, body), arg) if arg.isValue =>
+        Some(subst(x, arg, body))
+
+      case App(f, arg) if f.step.isDefined =>
+        Some(App(f.step.get, arg))
+
+      case App(f, arg) if f.isValue && arg.step.isDefined =>
+        Some(App(f, arg.step.get))
+
+      case If(cnd, thn, els) if cnd.isValue =>
+        cnd match {
+          case True()  => Some(thn)
+          case False() => Some(els)
+          case _       => None()
+        }
+
+      case If(cnd, thn, els) if cnd.step.isDefined =>
+        Some(If(cnd.step.get, thn, els))
+
+      case _ =>
+        None()
+    }
+
+    def reducesTo(t: Term, n: BigInt): Boolean = {
+      require(n >= 0)
+
+      decreases(n)
+
+      this == t || {
+        n > 0 &&
+        this.step.isDefined &&
+        this.step.get.reducesTo(t, n - 1)
+      }
+    }
+  }
+
+  final case class Var(id: BigInt)                     extends Term
+  final case class App(f: Term, arg: Term)             extends Term
+  final case class Abs(x: BigInt, ty: Ty, body: Term)  extends Term
+  final case class If(cnd: Term, thn: Term, els: Term) extends Term
+  final case class True()                              extends Term
+  final case class False()                             extends Term
+
+  def subst(x: BigInt, s: Term, t: Term): Term = t match {
+    case Var(y) if x == y  => s
+    case Var(y) if x != y  => t
+    case Abs(y, ty, t1)    => Abs(y, ty, if (x == y) t1 else subst(x, s, t1))
+    case App(f, arg)       => App(subst(x, s, f), subst(x, s, arg))
+    case If(cnd, thn, els) => If(subst(x, s, cnd), subst(x, s, thn), subst(x, s, els))
+    case True()            => True()
+    case False()           => False()
+  }
+
+  def step_example1(n: BigInt): Boolean = {
+    require(n > 0)
+
+    val t = App(Abs(0, TArrow(TBool(), TBool()), Var(0)), Abs(0, TBool(), Var(0)))
+    val s = Abs(0, TBool(), Var(0))
+
+    t.reducesTo(s, n)
+  }.holds
+
+  def step_example2(n: BigInt): Boolean = {
+    require(n > 0)
+
+    val t = If(True(), False(), True())
+    val s = False()
+
+    t.reducesTo(s, n)
+  }.holds
+
+  def inferType(c: Map[BigInt, Ty], t: Term): Option[Ty] = {
+    t match {
+      case Var(id) =>
+        c.get(id)
+
+      case Abs(x, ty, body) =>
+        inferType(c.updated(x, ty), body) match {
+          case Some(tyBody) => Some(TArrow(ty, tyBody))
+          case None()       => None()
+        }
+
+      case App(f, arg) =>
+        val tyFOpt   = inferType(c, f)
+        val tyArgOpt = inferType(c, arg)
+
+        if (tyFOpt.isDefined && tyArgOpt.isDefined) {
+          tyFOpt.get match {
+            case TArrow(a, b) if a == tyArgOpt.get => Some(b)
+            case _                                 => None()
+          }
+        } else {
+          None()
+        }
+
+      case If(cnd, thn, els) =>
+        val cndTy = inferType(c, cnd)
+        val thnTy = inferType(c, thn)
+        val elsTy = inferType(c, els)
+
+        (cndTy, thnTy, elsTy) match {
+          case (Some(TBool()), Some(t), Some(e)) if t == e =>
+            Some(t)
+
+          case _ =>
+            None()
+        }
+
+      case True() =>
+        Some(TBool())
+
+      case False() =>
+        Some(TBool())
+    }
+  }
+
+  def example_typing1: Boolean = {
+    val ty = inferType(Map[BigInt, Ty](), Abs(0, TBool(), Var(0)))
+    ty.isDefined && ty.get == TArrow(TBool(), TBool())
+  }.holds
+
+  def example_typing2: Boolean = {
+    val ty = inferType(Map[BigInt, Ty](), If(True(), False(), True()))
+    ty.isDefined && ty.get == TBool()
+  }.holds
+
+  def lemma_free_in_ctx(c: Map[BigInt, Ty], x: BigInt, t: Term): Boolean = {
+    require(t.fv.contains(x) && inferType(c, t).isDefined)
+
+    t match {
+      case Var(y) if x == y  =>
+        check(c.contains(x))
+
+      case Var(y) if x != y =>
+        check(c.contains(x))
+
+      case Abs(y, _, t1) if x == y =>
+        false
+
+      case Abs(y, ty, t1) if x != y =>
+        check(inferType(c.updated(y, ty), t1).isDefined) &&
+        check(t1.fv.contains(x)) &&
+        check(lemma_free_in_ctx(c.updated(y, ty), x, t1)) &&
+        check(c.contains(x))
+
+      case App(f, arg) =>
+        assert(f.fv.contains(x) ==> lemma_free_in_ctx(c, x, f))
+        assert(arg.fv.contains(x) ==> lemma_free_in_ctx(c, x, arg))
+        check(c.contains(x))
+
+      case If(cnd, thn, els) =>
+        assert(cnd.fv.contains(x) ==> lemma_free_in_ctx(c, x, cnd))
+        assert(thn.fv.contains(x) ==> lemma_free_in_ctx(c, x, thn))
+        assert(els.fv.contains(x) ==> lemma_free_in_ctx(c, x, els))
+
+        check(c.contains(x))
+
+      case True() =>
+        check(c.contains(x))
+
+      case False() =>
+        check(c.contains(x))
+    }
+  }.holds
+
+  @induct
+  def corollary_typeable_empty__closed(t: Term) = {
+    require(inferType(Map[BigInt, Ty](), t).isDefined)
+
+    assert {
+      forall { (x: BigInt) =>
+        t.fv.contains(x) ==> !lemma_free_in_ctx(Map[BigInt, Ty](), x, t)
+      }
+    }
+
+    t match {
+      case Abs(x, ty, body) =>
+        assert(inferType(Map[BigInt, Ty]().updated(x, ty), body).isDefined)
+        check(t.isClosed)
+
+      case t =>
+        check(t.isClosed)
+    }
+
+  }.holds
+
+  // @induct
+  def corollary_ctx_invariance(c: Map[BigInt, Ty], d: Map[BigInt, Ty], t: Term): Boolean = {
+    require {
+      inferType(c, t).isDefined &&
+      inferType(d, t).isDefined &&
+      forall { (x: BigInt) => t.fv.contains(x) ==> (c.get(x) == d.get(x)) }
+    }
+
+    t match {
+      case Var(y) =>
+        check(inferType(c, t).get == inferType(d, t).get)
+
+      case Abs(x, ty, body) =>
+        assert(inferType(c.updated(x, ty), body).isDefined)
+        assert(inferType(d.updated(x, ty), body).isDefined)
+        assert(corollary_ctx_invariance(c.updated(x, ty), d.updated(x, ty), body))
+        check(inferType(c, t).get == inferType(d, t).get)
+
+      case App(f, arg) =>
+        assert(corollary_ctx_invariance(c, d, f))
+        assert(corollary_ctx_invariance(c, d, arg))
+        check(inferType(c, t).get == inferType(d, t).get)
+
+      case If(cnd, thn, els) =>
+        assert(corollary_ctx_invariance(c, d, cnd))
+        assert(corollary_ctx_invariance(c, d, thn))
+        assert(corollary_ctx_invariance(c, d, els))
+
+        check(inferType(c, t).get == inferType(d, t).get)
+
+      case True() =>
+        check(inferType(c, t).get == inferType(d, t).get)
+
+      case False() =>
+        check(inferType(c, t).get == inferType(d, t).get)
+    }
+
+  }.holds
+
+}
+
