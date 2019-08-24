6451f7614fd10db4fec55e5fc1af6b2804cdc347
Romain Ruetschi <romain.ruetschi@gmail.com>
2019-08-24 15:36:27 +0200

    Fix run-tests.sh

--- run-tests.sh
+++ run-tests.sh
@@ -2,20 +2,29 @@
 
 STAINLESS_SCALAC="stainless-scalac --timeout=300 --vc-cache=false --fail-early"
 
-STD_TESTS="algorithms extended-gcd fp-principles Fulcrum gcd LambdaEvaluator LeftPad reachabilityChecker software-foundations stlc"
+STD_TESTS="algorithms extended-gcd fp-principles Fulcrum gcd LeftPad reachabilityChecker software-foundations"
 
 function run_tests {
   option=$1
   project=$2
+
+  echo ""
+  echo "------------------------------------------------------------------"
   echo "Running stainless-scalac on bolts project: $project..."
   find "$project" -name '*.scala' -exec $STAINLESS_SCALAC $option {} +
+
   status=$?
 
   if [ $status -ne 0 ]
   then
     echo "$STAINLESS_SCALAC failed on bolts project: $project."
+    echo "------------------------------------------------------------------"
+    echo ""
     exit 1
   fi
+
+  echo "------------------------------------------------------------------"
+  echo ""
 }
 
 for project in $STD_TESTS; do
@@ -24,7 +33,7 @@ done
 
 # The `--type-checker` option does not support `forall` so all files containing `forall` are ignored.
 
-TC_TESTS="fp-principles Fulcrum LambdaEvaluator LeftPad reachabilityChecker software-foundations stlc"
+TC_TESTS="fp-principles Fulcrum LeftPad reachabilityChecker software-foundations"
 
 for project in $TC_TESTS; do
   run_tests "--type-checker" "$project"
