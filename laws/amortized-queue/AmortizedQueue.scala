db5f108f365d1fae3fee4c8fc8e7333d1d43060f
Viktor Kuncak <viktor.kuncak@epfl.ch>
2020-09-30 14:48:00 +0200

    repeated comments for those looking at this file without further context

--- laws/amortized-queue/AmortizedQueue.scala
+++ laws/amortized-queue/AmortizedQueue.scala
@@ -54,3 +54,16 @@ case class AmortizedQueue[A](front: List[A], rear: List[A]) extends Queue[A] {
 
   def toList = front ++ rear.reverse
 }
+
+/*
+=== Functional Queues === 
+
+Recall that a queue allows taking elements from the **front** and adding elements to the **end**.  Queues are expected to implement these operations in //about constant// time. Here we are going to implement and prove correct a //purely functional// queue, which means that it does not use mutation. The benefit is that we can prove such implementations correct much more easily and, moreover, even after adding or removing an element we can continue to access the previous version of the queue. On the flip side, it means that a simple imperative implementation (such as a doubly linked list with a tail pointer) is out of question. Instead, we use functional lists. A simplest functional approach would be to implement dequeue as list head and implement enqueue as appending a single element to the end of the list. Unfortunately, this would make enqueue linear in the size of the queue. Conversely, if we decided to implement dequeue by removing from the end of a list, then the dequeue would take linear time, instead of constant. A solution is instead to use two lists: one for removing and one for adding. When the list from which remove gets empty, we need to add elements from the other list. We do this by reversing the list, which is good on average, because we do it only once for every element we put into the queue.
+
+=== Functional Queues in Stainless ===
+
+The example contains three class definitions:
+  - First, we define **abstract class** `Queue` whose method bodies are only placeholders, but, crucially, contain postconditions that every implementation must obey. These postconditions are specified using `toList` function, which itself is abstract. Such functions used in specifications are called **abstraction function**: they map a (possibly complex) implementation into a simpler model (in this case list).
+  - As an illustration, `SimpleQueue` shows a trivial implementation, where `toList` is an identity function. This gives us the first (inefficient) implementation. Note that, even though `SimpleQueue` does not contain any specifications, Stainless automatically checks that all postconditions of the superclass hold. (You can test this by trying to make implementation incorrect and examining what happens).
+  - Finally, we define our amortized implementation that has a `front` list and `rear` list. The `toList` method gives the meaning of our queue in terms of list: it is meant to represent `front ++ rear.reverse`. In case of `enqueue` we provide a proof, using Stainless' `equations` library, that establishes the postcondition stated in the abstract class.
+ */
