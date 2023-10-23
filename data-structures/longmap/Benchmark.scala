a0159abc874e9906bb3a6356890b2ca5f709ab9c
Samuel Chassot <samuel.chassot@epfl.ch>
2023-10-23 09:31:41 +0200

    benchmark

--- data-structures/longmap/Benchmark.scala
+++ data-structures/longmap/Benchmark.scala
@@ -2,22 +2,34 @@
 // scala-cli -J -Xmx24576m Benchmark.scala MutableLongMap.scala StrictlyOrderedLongListMap.scala  $(find /localhome/chassot/stainless/frontends/library/stainless/ -name "*.scala")
 
 import ch.epfl.chassot.MutableLongMap.*
+import scala.collection.mutable.LongMap
 
 object MutableLongMapBenchmark {
-  def benchmark() = {
+  def benchmarkVerifiedMap(n: Int) = {
     val m = getEmptyLongMap(l => 0L)
     // val n = Math.pow(2, 10).toInt
-    val n = 127
     val t1 = System.nanoTime()
     for (i <- 0 to n) {
       val res = m.update(i, i)
-      println(f"Inserting $i: $res")
     }
     val t2 = System.nanoTime()
-    println(f"Time to insert $n elements: ${(t2 - t1) / 1000000} ms")
+    println(f"Time to insert $n elements in verified: ${(t2 - t1) / 1000000} ms")
+  }
+
+  def benchmarkOriginalMap(n: Int) = {
+    val m = LongMap[Long]()
+    // val n = Math.pow(2, 10).toInt
+    val t1 = System.nanoTime()
+    for (i <- 0 to n) {
+      val res = m.update(i, i)
+    }
+    val t2 = System.nanoTime()
+    println(f"Time to insert $n elements in original: ${(t2 - t1) / 1000000} ms")
   }
 }
 
 @main def main(): Unit = {
-  MutableLongMapBenchmark.benchmark()
+  val n = 4096
+  MutableLongMapBenchmark.benchmarkVerifiedMap(n)
+  MutableLongMapBenchmark.benchmarkOriginalMap(n)
 }
