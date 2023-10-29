62f29eaabefa114642be9ae84985d79b6d0b9461
Viktor Kuncak <viktor.kuncak@epfl.ch>
2023-10-29 17:46:07 +0100

    Height computed

--- data-structures/trees/simple-conc/SimpleConc.scala
+++ data-structures/trees/simple-conc/SimpleConc.scala
@@ -7,9 +7,6 @@ import ListSpecs._
 import stainless.annotation._
 
 object SimpleConc:
-  def max(x: BigInt, y: BigInt) =
-    if x < y then y else x
-
   trait Seq[T]:
     def toList: List[T]
     def size: Int
@@ -21,34 +18,38 @@ object SimpleConc:
   sealed abstract class Conc[T]
   case class Empty[T]() extends Conc[T]
   case class Leaf[T](x: T) extends Conc[T]
-  case class Node[T](left: Conc[T], right: Conc[T], csize: BigInt) extends Conc[T] {
-    require(csize == left.size + right.size && left != Empty[T]() && right != Empty[T]())
+  case class Node[T](left: Conc[T], right: Conc[T], csize: BigInt, cheight: BigInt) extends Conc[T] {
+    require(csize == left.size + right.size && left != Empty[T]() && right != Empty[T]() &&
+            cheight == max(left.height, right.height) + 1)
   }
+
+  def max(x: BigInt, y: BigInt) =
+    if x < y then y else x
 
   extension[T](t: Conc[T])
     def toList: List[T] = t match
       case Empty() => Nil[T]()
       case Leaf(x) => List(x)
-      case Node(l, r, _) => l.toList ++ r.toList
+      case Node(l, r, _, _) => l.toList ++ r.toList
 
     def size: BigInt = {
       t match
         case Empty() => BigInt(0)
         case Leaf(_) => BigInt(1)
-        case Node(_, _, csize) => csize
+        case Node(_, _, csize, _) => csize
     }.ensuring(_ == t.toList.size)
 
     def height: BigInt = 
       t match 
         case Empty() => 0
         case Leaf(x) => 1
-        case Node(l, r, _) => 1 + max(l.height, r.height)
+        case Node(_, _, _, cheight) => cheight
 
     def apply(i: BigInt): T = {
       require(0 <= i && i < t.size)
       t match
         case Leaf(x) => assert(i == 0); x
-        case Node(l, r, _) =>
+        case Node(l, r, _, _) =>
           appendIndex(l.toList, r.toList, i) // lemma
           if i < l.size then l(i)
           else r(i - l.size)
@@ -58,7 +59,7 @@ object SimpleConc:
     def <>(t2: Conc[T]) = 
       if t1 == Empty[T]() then t2
       else if t2 == Empty[T]() then t1
-      else Node(t1, t2, t1.size + t2.size)
+      else Node(t1, t2, t1.size + t2.size, max(t1.height, t2.height) + 1)
 
     def ++(t2: Conc[T]): Conc[T] = {
       t1 <> t2
@@ -74,7 +75,7 @@ object SimpleConc:
           case Leaf(x) => 
             if from == 0 && until == 1 then Leaf(x)
             else Empty[T]()
-          case Node(l, r, _) =>
+          case Node(l, r, _, _) =>
             sliceLemma(l.toList, r.toList, from, until) // lemma
             if l.size <= from then r.slice(from - l.size, until - l.size)
             else if until <= l.size then l.slice(from, until)
@@ -90,7 +91,7 @@ object SimpleConc:
       t match
         case Empty() => Vector("Empty()")
         case Leaf(x) => Vector("Leaf(" + x.toString + ")")
-        case Node(l, r, csize) =>
+        case Node(l, r, csize, _) =>
           if csize <= 4 then
             Vector("Node(" + l.toStr.head + ", " + r.toStr.head + ")")
           else  
@@ -111,14 +112,14 @@ object SimpleConc:
       t match
         case Empty() => Vector("()")
         case Leaf(x) => Vector(x.toString)
-        case Node(l, r, csize) =>
+        case Node(l, r, csize, _) =>
           val ls = l.toDraw
           val rs = r.toDraw         
           val p = 3* ls.size / 2 // push first subtree right for visual balance
-          val ls1 = Vector("─┬─" + "─"*p + ls.head) ++
-                    ls.tail.map(" │ " + " "*p + _)
-          val rs1 = Vector(" └─" + rs.head) ++
-                    rs.tail.map("   " + _)
+          val ls1 = Vector("┬" + "─"*p + ls.head) ++
+                    ls.tail.map("│" + " "*p + _)
+          val rs1 = Vector("└" + rs.head) ++
+                    rs.tail.map(" " + _)
           ls1 ++ rs1
     end toDraw
 
@@ -127,15 +128,27 @@ object SimpleConc:
   def show[T](t: Conc[T]): String = 
     t.toDraw.mkString("\n")
 
+  def mkTree(from: Int, until: Int): Conc[Int] =
+    require(0 <= from && from <= until && until <= 1_000_000)
+    decreases(until - from)
+    if from == until then Empty()
+    else if from + 1 == until then Leaf(from)
+    else
+      val mid = from + (until - from)/2
+      mkTree(from, mid) <> mkTree(mid, until)    
+
   @main @extern
   def test =
-    val c1: Conc[Int] = (1 to 8).map(Leaf(_)).foldLeft[Conc[Int]](Empty())((a, b) => a <> b)
+    val c1: Conc[Int] = (1 to 8).map(Leaf(_)).foldRight[Conc[Int]](Empty())((a, b) => a <> b)
+    println(f"\nc1.height = ${c1.height}")
     println(show(c1))
-    println(f"c1.height = ${c1.height}")
-    val c2: Conc[Int] = (1 to 3).foldRight[Conc[Int]](Leaf(4))((_,t) => t <> t)
+    val c2: Conc[Int] = (1 to 8).map(Leaf(_)).foldLeft[Conc[Int]](Empty())((a, b) => a <> b)
+    println(f"\nc2.height = ${c2.height}")
     println(show(c2))
-    println(f"c2.height = ${c2.height}")
 
+    val c3 = mkTree(1, 9)
+    println(f"\nc3.height = ${c3.height}")
+    println(show(c3))
 
 
   // **************************************************************************
