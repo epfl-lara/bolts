c21d4b49cfed332a3ccf8112ffa06bc02400b825
Samuel Chassot <samuel.chassot@epfl.ch>
2023-11-02 10:39:17 +0100

    new benchmark (more values + array )

--- data-structures/longmap/mutablemap/src/main/scala/Main.scala
+++ data-structures/longmap/mutablemap/src/main/scala/Main.scala
@@ -1,6 +1,7 @@
 import ch.epfl.chassot.MutableLongMap
 import ch.epfl.chassot.ListMapLongKey
 import stainless.collection.List
+import benchmark.BenchmarkUtil.*
 
 object Main extends App{
   def main(): Unit = {
@@ -8,14 +9,29 @@ object Main extends App{
 
       var mTest = ListMapLongKey[Long](List[(Long, Long)]())
 
-      val max = 512L
-      val keys = (0L until max).toList
+
+      val keysListName = (2048*8).toString()
       val t1 = System.nanoTime()
-      for i <- keys do
-        mTest = mTest + (i, i)
+      // val map = getOriginalMapEmptyBuffer(16)
+      val map = getVerifiedMapEmptyBuffer(16)
+      for (k, v) <- random2to15Pairs do
+          map.update(k, v)
+      end for
+
+      for k <- getKeysList("24576") do
+          map.remove(k)
+      end for
+
+      for (k, v) <- random2to15Pairs do
+          map.update(k, v)
+      end for
+
+      for k <- getKeysList(keysListName) do
+          map(k)
+      end for
       val t2 = System.nanoTime()
       // val res = keys.map(k => (mTest(k) == k)).reduce(_ && _)
-      println(f"Time to populate ${keys.size} key-value pairs: ${(t2 - t1) / 1e9}%.2f secs")
+      println(f"Time to run ${(t2 - t1) / 1e6}%.2f ms")
       // println(f"Result valiity: $res")
 
       
