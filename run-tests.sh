c363f887081782ce765962b36b5d5e1b982cb118
mario-bucev <mario.bucev@epfl.ch>
2023-11-07 12:07:07 +0100

    Add 'Scala 3 only' benchmarks in running scripts (#74)

--- run-tests.sh
+++ run-tests.sh
@@ -29,11 +29,23 @@ function run_tests {
   echo ""
 }
 
-# Tests that use the usual --type-checker=true verification condition generator:
+# Determining the frontend version
+OUTPUT=$($STAINLESS --version --no-colors)
+VERSION=$(echo "${OUTPUT}" | awk '/^Bundled Scala compiler: /{print $4}')
+IS_DOTTY=false
+if [[ $VERSION == 3\.* ]]; then
+  IS_DOTTY=true
+elif [[ $VERSION != 2\.* ]]; then
+  echo "Could not recognize Stainless frontend version"
+  exit 1
+fi
 
 TC_TESTS=`cat tctests.txt`
-echo **************************
-echo Type Checking vcgen tests:
+if [[ "$IS_DOTTY" = true ]]; then
+  TC_TESTS=$(cat tctests_dotty.txt)$'\n'$TC_TESTS
+fi
+echo "**************************"
+echo "Type Checking vcgen tests:"
 echo $TC_TESTS
 for project in $TC_TESTS; do
   run_tests "$project" "--config-file=stainless.conf.nightly"
