32e9fc25c985b704faf72ace05c520e321cc8f6c
Samuel Chassot <samuel.chassot@epfl.ch>
2024-08-05 14:15:42 +0200

    change spec to be pre and post condition, and private cache

--- caching/CachedFunction.scala
+++ caching/CachedFunction.scala
@@ -10,6 +10,8 @@ object CachedFunction {
     CachedFunction(f, hashable, MutableHashMap.getEmptyHashMap[I, O](f, hashable))
   }
 
+  @pure @ghost def isImageOf[I, O](f: I => O): ((I, O)) => Boolean = (t) => t._2 == f(t._1) // If we use a lambda, it is rejected for illegal capture of thiss
+
   @ghost
   @inline
   def allValuesAreFunctionOutputs[I, O](
@@ -17,7 +19,7 @@ object CachedFunction {
       cache: MutableHashMap.HashMap[I, O]
   ): Boolean = {
     require(cache.valid)
-    cache.map.forall((k, v) => v == f(k))
+    cache.map.forall(isImageOf[I, O](f))
   }
 
   @ghost
@@ -40,7 +42,7 @@ object CachedFunction {
         y
       )
       assert(cache.map.toList.contains((x, cache(x))))
-      ListSpecs.forallContained(cache.map.toList, (k, v) => v == f(k), (x, y))
+      ListSpecs.forallContained(cache.map.toList, isImageOf[I, O](f), (x, y))
   }.ensuring (_ => if (cache.contains(x)) then cache(x) == f(x) else true)
 
 }
@@ -48,21 +50,22 @@ object CachedFunction {
 final case class CachedFunction[I, O](
     f: I => O,
     hashable: Hashable[I],
-    cache: MutableHashMap.HashMap[I, O]
+    private val cache: MutableHashMap.HashMap[I, O]
 ) {
-  require(cache.valid)
-  require(CachedFunction.allValuesAreFunctionOutputs(f, cache))
-
   @opaque
   def apply(x: I): O = {
+    require(cache.valid)
+    require(CachedFunction.allValuesAreFunctionOutputs(f, cache))
     if cache.contains(x) then
       ghostExpr(CachedFunction.lemmaInMapThenCorrect(f, cache, x, cache(x)))
       cache(x)
     else
       val result = f(x)
+      @ghost val thiss = snapshot(this)
+      ghostExpr(MutableHashMap.lemmaUpdatePreservesForallPairs(cache, x, result, CachedFunction.isImageOf[I, O](f)))
       cache.update(x, result)
       result
-  }.ensuring { res => res == f(x) }
+  }.ensuring { res => res == f(x) && cache.valid && CachedFunction.allValuesAreFunctionOutputs(f, cache)}
 }
 
 // object IntHashable extends Hashable[Int] {
