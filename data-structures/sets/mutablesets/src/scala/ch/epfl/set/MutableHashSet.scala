78e5816916e8e61e4022d94533a78a37792e6b8b
Samuel Chassot <samuel.chassot@epfl.ch>
2024-08-15 17:01:01 +0200

    working on listmap to use it for hashset

--- data-structures/sets/mutablesets/src/scala/ch/epfl/set/MutableHashSet.scala
+++ data-structures/sets/mutablesets/src/scala/ch/epfl/set/MutableHashSet.scala
@@ -1,16 +1,17 @@
 package ch.epfl.set
 
-import stainless.annotation._
-import stainless.collection._
-import stainless.lang._
+import stainless.annotation.*
+import stainless.collection.*
+import stainless.lang.{ghost => ghostExpr, *}
 
 import stainless.lang.StaticChecks.* // Comment out when using the OptimisedEnsuring object below
 
 import ch.epfl.map.MutableMapInterface.iMHashMap
+import ch.epfl.map.ListMap
 
-object MutableHashSetObj:
 
-  case class MutableHashSet[V](private val underlying: iMHashMap[V, Boolean]) extends MutableSetInterface.iMSet[V]:
+case class MutableHashSet[V](private val underlying: iMHashMap[V, Boolean]) extends MutableSetInterface.iMSet[V]:
+  import MutableHashSetObj.*
     @pure 
     @ghost
     override def valid: Boolean = {
@@ -22,12 +23,24 @@ object MutableHashSetObj:
     }
 
     override def contains(v: V): Boolean = {
+      require(valid)
+      ghostExpr(lemmaUnderlyingContainsSamAsAsbtractSetContains(underlying.abstractMap, v))
       underlying.contains(v)
     }.ensuring(res => valid && (res == abstractSet.contains(v)))
 
     override def update(v: V): Boolean = {
       require(valid)
-      underlying.update(v, true)
+      @ghost val oldAbstractMap = underlying.abstractMap
+      @ghost val oldAbstractSet = abstractSet
+      assert(abstractSet == extractSet(oldAbstractMap.toList))
+      val res = underlying.update(v, true)
+      if res then
+        assert(underlying.abstractMap.eq(oldAbstractMap + (v, true)))
+        ghostExpr(lemmaNewPairListMapInAbstractSet(oldAbstractMap, v))
+        assert(abstractSet.contains(v))
+        assert(abstractSet == oldAbstractSet + v)
+      assert(valid)
+      res
     }.ensuring(res => valid && (if (res) then abstractSet.contains(v) && (abstractSet == old(this).abstractSet + v) else abstractSet == old(this).abstractSet))
 
     override def remove(v: V): Boolean = {
@@ -36,23 +49,47 @@ object MutableHashSetObj:
     }.ensuring(res => valid && (if (res) then abstractSet == old(this).abstractSet - v else abstractSet == old(this).abstractSet))
 
     override def isEmpty: Boolean = {
+      require(valid)
       underlying.isEmpty
-    }.ensuring(res => valid && (res == abstractSet.isEmpty))
+    }
 
     @ghost
     @pure
     override  def abstractSet: Set[V] = {
       require(valid)
-      extractSet(underlying.abstractMap.toList)
+      underlying.abstractMap.keys().content
     }
 
-  end MutableHashSet
+end MutableHashSet
+
+
+object MutableHashSetObj:
 
   def extractSet[V](m: List[(V, Boolean)]): Set[V] = {
     m match
       case Cons((k, _), tl) => extractSet(tl) + k
       case Nil() => Set()
   }
+
+  @ghost
+  @inlineOnce
+  @opaque
+  def lemmaUnderlyingContainsSamAsAsbtractSetContains[V](lm: ListMap[V, Boolean], v: V): Unit = {
+    decreases(lm)
+    lm.toList match 
+      case Cons((k, _), tl) => lemmaUnderlyingContainsSamAsAsbtractSetContains(ListMap(tl), v)
+      case Nil() =>
+  }.ensuring(_ => extractSet(lm.toList).contains(v) == lm.contains(v))
+
+  @ghost
+  @inlineOnce
+  @opaque
+  def lemmaNewPairListMapInAbstractSet[V](lm: ListMap[V, Boolean], k: V): Unit = {
+    decreases(lm)
+    lm.toList match 
+      case Cons((k1, v1), tl) => lemmaNewPairListMapInAbstractSet(ListMap(tl), k)
+      case Nil() =>
+  }.ensuring(_ => extractSet((lm + (k, true)).toList) == extractSet(lm.toList) + k)
 
 
 end MutableHashSetObj
