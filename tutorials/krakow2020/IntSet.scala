f43f4420a5aab5edaa53f50ea0f92451c9e5691e
Samuel Chassot <samuel.chassot@epfl.ch>
2024-08-23 11:47:00 +0200

    migration to 3.5

--- tutorials/krakow2020/IntSet.scala
+++ tutorials/krakow2020/IntSet.scala
@@ -9,21 +9,21 @@ sealed abstract class IntSet {
     def incl(x: Int): IntSet = this match {
         case Empty() => Node(Empty(),x,Empty())
         case Node(left, elem, right) =>
-          if (x < elem) Node(left incl x, elem, right)
-          else if (x > elem) Node(left, elem, right incl x)
+          if (x < elem) Node(left.incl(x), elem, right)
+          else if (x > elem) Node(left, elem, right.incl(x))
           else this
     }
     def contains(x: Int): Boolean = this match {
         case Empty() => false
         case Node(left, elem, right) =>
-          if (x < elem) left contains x
-          else if (x > elem) right contains x
+          if (x < elem) left.contains(x)
+          else if (x > elem) right.contains(x)
           else true
     }
 
     def P1(x: Int): Boolean = {
         true
-    }.ensuring(_ => !(Empty() contains x))
+    }.ensuring(_ => !(Empty().contains(x)))
 
     def P2(s: IntSet, x: Int): Boolean = {
         s match {
@@ -33,7 +33,7 @@ sealed abstract class IntSet {
                 else if (x > elem) P2(right, x)
                 else true
         }
-    }.ensuring(_ => (s incl x) contains x)
+    }.ensuring(_ => (s.incl(x)).contains(x))
 
     def P3(s: IntSet, x: Int,  y: Int): Boolean = {
         require(x != y)
@@ -44,11 +44,11 @@ sealed abstract class IntSet {
                 else if (x > elem) P3(right, x, y)
                 else true
         }
-    }.ensuring(_ =>  ((s incl x) contains y)==(s contains y))
+    }.ensuring(_ =>  ((s.incl(x)).contains(y))==(s.contains(y)))
 
     def union(other: IntSet): IntSet = this match {
         case Empty() => other
         case Node(left, x, right) => 
-            (left union (right union other)) incl x
+            (left.union(right.union(other))).incl(x)
     }
 }
