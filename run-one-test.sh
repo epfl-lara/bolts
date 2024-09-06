f7116ed40a89a9d49ac103451feb31354da336f3
Samuel Chassot <samuel.chassot@epfl.ch>
2024-09-06 10:58:05 +0200

    run stainless with admit VCs in the CI

--- run-one-test.sh
+++ run-one-test.sh
@@ -1,5 +1,10 @@
 #!/usr/bin/env bash
 
+ADMIT_VCS=false
+if [[ "$1" = "--admit-vcs" ]]; then
+  ADMIT_VCS=true
+  shift # Remove the flag from the arguments
+fi
 STAINLESS="$1"
 
 function run_tests {
@@ -23,27 +28,44 @@ function run_tests {
     echo "$ cat ./verify.sh"
     cat "./verify.sh"
     echo ""
-    bash "./verify.sh"
+    bash "./verify.sh" "--admit-vcs=true"
     status=$?
     cd -
   else
     echo "Running '$STAINLESS --config-file=$conf $@' on bolts project: $project..."
     echo "$ find $project -name '*.scala' -exec $STAINLESS --config-file=$conf $@ {} +"
-    find "$project" -name '*.scala' -exec $STAINLESS "--config-file=$conf" "$@" {} +
+    if [ "$ADMIT_VCS" = true ]; then
+      find "$project" -name '*.scala' -exec $STAINLESS "--config-file=$conf" "--admit-vcs=true" "$@" {} +
+    else
+      find "$project" -name '*.scala' -exec $STAINLESS "--config-file=$conf" "$@" {} +
+    fi
   fi
 
   status=$?
 
-  if [ $status -ne 0 ]
-  then
-    echo "'$STAINLESS $@' failed on bolts project: $project."
+  if [ $ADMIT_VCS = true ]; then
+    if [ $status -eq 0 ] || [ $status -eq 1 ]; then
+      echo "Stainless accepted project: $project."
+      exit 0
+    else
+      echo "Stainless rejected project: $project."
+      exit 1
+    fi
     echo "------------------------------------------------------------------------------------------"
     echo ""
-    exit 1
+    return $status
+  else
+    if [ $status -ne 0 ]
+    then
+      echo "'$STAINLESS $@' failed on bolts project: $project."
+      echo "------------------------------------------------------------------------------------------"
+      echo ""
+      exit 1
+    fi
+    echo "------------------------------------------------------------------------------------------"
+    echo ""
+    exit 0
   fi
-
-  echo "------------------------------------------------------------------------------------------"
-  echo ""
 }
 
 run_tests "${@:2}"
