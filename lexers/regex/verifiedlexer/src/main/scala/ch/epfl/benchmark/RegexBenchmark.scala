d690c127862dde2650d9058002f38b943bb24b22
Samuel Chassot <14821693+samuelchassot@users.noreply.github.com>
2025-01-15 14:26:43 +0100

    Adding Zippers implementation and verification, and some benchmarks (#116)
    * add sbt project with plugin
    
    * move regex to an sbt project
    
    * original version
    
    * working on the memoisation
    
    * not working
    
    * add longMap + work on cache
    
    * for now issue with illegal capturing
    
    * to specific, not working but almost
    
    * working on memoised regex
    
    * working
    
    * cached version proven
    
    * working examples
    
    * new hashmap without ordering
    
    * add new lemmas in ListMaps for forall
    
    * Formally verified cache for regex
    
    * add removeUselessConcat simplification for regex + equivalence proof
    
    * add a simplification function that is sound
    
    * working on regex
    
    * as symlink
    
    * ensuring as method and not infix
    
    * implementation of Zippers (with List for Set, I'm changing to Set to try)
    
    * continue working on zippers
    implementation seems done, now working on the proof
    
    * working on zipper
    
    * remove chassot pacakge, obsolete
    
    * finalise merge
    
    * add hashset symlink
    
    * add a few imports
    
    * start to migrate to sets
    
    * continue replacing list by set
    
    * Move to Stainless Set, passing tests
    
    * working on zippers
    
    * working
    
    * remove useless imports
    
    * work on zippers
    
    * add some lemmas about flatmap
    
    * add getwitness for sets
    
    * working on zppier
    
    * working
    
    * change to ghost
    
    * change Context to be a class with invariant to emulate refinement types
    
    * Prove that the derivation of zippers is associative
    
    * working on zipper proof
    
    * proving stuff
    
    * proving equivalence of base cases
    
    * add generalisations of union and concat to base regex
    
    * work on regex
    
    * revert the generalised regex
    
    * add generalised union and concat as method
    
    * matchRGenUnionSpec and matchRGenConcatSpec
    
    * working on zipper proof
    
    * working on Zipper theorem
    
    * working on zipper proof
    
    * working on big proof direct induction - not working for star
    
    * working on zipper proof
    
    * working on the zipper proof
    
    * working on zipper
    
    * proved lemmaConcatZipperMatchesStringThenFindConcatDefined
    
    * working on zipper proof, almost done
    
    * working on zipper proof, almost done
    
    * done proving ZIPERRR :D
    
    * clean up
    
    * Add main regex matching function to use zipper, rename theorem
    
    * add benchmark + memoisation as extern for flatmap
    
    * add verification for memoisation
    
    * add benchmarks and some results
    
    * update CI
    
    * debug ci
    
    * debug ci
    
    * debug
    
    * ci
    
    * add memoised matching using Zipper to regex
    
    * benchmark + typo
    
    * rename for future benchmark analysis
    
    * benchmark data
    
    * remove one main test
    
    * new benchmark
    
    * typo
    
    * fix the ci
    
    ---------
    
    Co-authored-by: Samuel Chassot <samuel@tsf-476-wpa-1-098.epfl.ch>

---
+++ lexers/regex/verifiedlexer/src/main/scala/ch/epfl/benchmark/RegexBenchmark.scala
@@ -0,0 +1,191 @@
+package benchmark
+
+import java.util.concurrent.TimeUnit
+import org.openjdk.jmh.annotations.*
+import scala.util.Random
+import stainless.collection.{List => StainlessList}
+import ch.epfl.lexer.VerifiedRegex.Regex
+import ch.epfl.benchmark.RegexUtils.*
+import scala.util.Random
+import scala.compiletime.uninitialized
+import ch.epfl.lexer.VerifiedRegexMatcher.matchZipper
+import ch.epfl.lexer.VerifiedRegexMatcher.matchR
+import ch.epfl.lexer.VerifiedRegexMatcher.matchRMem
+import ch.epfl.lexer.VerifiedRegexMatcher.matchZipperMem
+import ch.epfl.lexer.MemoisationRegex
+import ch.epfl.lexer.MemoisationZipper
+import ch.epfl.lexer.ZipperRegex.Context
+import ch.epfl.map.Hashable
+
+@State(Scope.Benchmark)
+class RegexBenchmark {
+
+  @Param(
+    Array(
+     "5", 
+     "10", 
+     "15", 
+     "20", 
+     "25", 
+     "30", 
+     "35", 
+     "40", 
+     "45", 
+     "50", 
+     "55", 
+     "60", 
+     "65", 
+     "70", 
+     "75", 
+     "80",
+     "85",
+     "90",
+     "95", 
+     "100"
+    )
+  )
+  var size: String = uninitialized
+
+  @Benchmark
+  @BenchmarkMode(Array(Mode.AverageTime))
+  @OutputTimeUnit(TimeUnit.MICROSECONDS)
+  def abStarAccepting_Regex(): Unit = {
+    val r = RegexBenchmarkUtil.abStar
+    val s = RegexBenchmarkUtil.abStar_Accepting_strings(size.toInt)
+    val res = matchR(r, s)
+    assert(res)
+  }
+
+  @Benchmark
+  @BenchmarkMode(Array(Mode.AverageTime))
+  @OutputTimeUnit(TimeUnit.MICROSECONDS)
+  def abStarAccepting_Zipper(): Unit = {
+    val r = RegexBenchmarkUtil.abStar
+    val s = RegexBenchmarkUtil.abStar_Accepting_strings(size.toInt)
+    val res = matchZipper(r, s)
+    assert(res)
+  }
+
+  @Benchmark
+  @BenchmarkMode(Array(Mode.AverageTime))
+  @OutputTimeUnit(TimeUnit.MICROSECONDS)
+  def abStarAccepting_RegexMem(): Unit = {
+    val r = RegexBenchmarkUtil.abStar
+    val s = RegexBenchmarkUtil.abStar_Accepting_strings(size.toInt)
+    val res = matchRMem(r, s)(RegexBenchmarkUtil.regexCache)
+    assert(res)
+  }
+
+  @Benchmark
+  @BenchmarkMode(Array(Mode.AverageTime))
+  @OutputTimeUnit(TimeUnit.MICROSECONDS)
+  def abStarAccepting_ZipperMem(): Unit = {
+    val r = RegexBenchmarkUtil.abStar
+    val s = RegexBenchmarkUtil.abStar_Accepting_strings(size.toInt)
+    val res = matchZipperMem(r, s)(RegexBenchmarkUtil.zipperCacheUp, RegexBenchmarkUtil.zipperCacheDown)
+    assert(res)
+  }
+
+  // Email accepting regex -----------------------------------------------------------------------------------------------------------------------
+
+  @Benchmark
+  @BenchmarkMode(Array(Mode.AverageTime))
+  @OutputTimeUnit(TimeUnit.MICROSECONDS)
+  def emailAccepting_Regex(): Unit = {
+    val r = RegexBenchmarkUtil.emailRegex
+    val s = RegexBenchmarkUtil.email_Accepting_strings(size.toInt)
+    val res = matchR(r, s)
+    assert(res)
+  }
+
+  @Benchmark
+  @BenchmarkMode(Array(Mode.AverageTime))
+  @OutputTimeUnit(TimeUnit.MICROSECONDS)
+  def emailAccepting_Zipper(): Unit = {
+    val r = RegexBenchmarkUtil.emailRegex
+    val s = RegexBenchmarkUtil.email_Accepting_strings(size.toInt)
+    val res = matchZipper(r, s)
+    assert(res)
+  }
+
+  @Benchmark
+  @BenchmarkMode(Array(Mode.AverageTime))
+  @OutputTimeUnit(TimeUnit.MICROSECONDS)
+  def emailAccepting_ZipperMem(): Unit = {
+    val r = RegexBenchmarkUtil.emailRegex
+    val s = RegexBenchmarkUtil.email_Accepting_strings(size.toInt)
+    val res = matchZipper(r, s)
+    assert(res)
+  }
+
+  @Benchmark
+  @BenchmarkMode(Array(Mode.AverageTime))
+  @OutputTimeUnit(TimeUnit.MICROSECONDS)
+  def emailAccepting_RegexMem(): Unit = {
+    val r = RegexBenchmarkUtil.emailRegex
+    val s = RegexBenchmarkUtil.email_Accepting_strings(size.toInt)
+    val res = matchRMem(r, s)(RegexBenchmarkUtil.regexCache)
+    assert(res)
+  }
+
+ 
+}
+
+object RegexCharHashable extends Hashable[(Regex[Char], Char)] {
+  override def hash(k: (Regex[Char], Char)): Long = {
+    val (r, c) = k
+    r.hashCode() * 31 + c.hashCode()
+  }
+}
+
+object ContextCharHashable extends Hashable[(Context[Char], Char)] {
+  override def hash(k: (Context[Char], Char)): Long = {
+    val (ctx, c) = k
+    ctx.hashCode() * 31 + c.hashCode()
+  }
+}
+
+object RegexContextCharHashable extends Hashable[(Regex[Char], Context[Char], Char)] {
+  override def hash(k: (Regex[Char], Context[Char], Char)): Long = {
+    val (r, ctx, c) = k
+    r.hashCode() * 63 + ctx.hashCode() * 31 + c.hashCode()
+  }
+}
+
+object RegexBenchmarkUtil {
+  val seed = 0x0ddba11
+  val r = new Random(seed)
+
+  val string_sizes = List(5, 10, 15, 20, 25, 30, 35, 40, 45, 50, 55, 60, 65, 70, 75, 80, 85, 90, 95, 100, 150, 200)
+  val abStar: Regex[Char] = ("a" | "b").*
+
+  val abStar_Accepting_strings: Map[Int, StainlessList[Char]] = string_sizes.map(n => (n, (1 to n).map(_ => random_a_or_b()).mkString.toStainless)).toMap
+
+  val regexCache: MemoisationRegex.Cache[Char] = MemoisationRegex.empty(RegexCharHashable)
+  val zipperCacheUp: MemoisationZipper.CacheUp[Char] = MemoisationZipper.emptyUp(ContextCharHashable)
+  val zipperCacheDown: MemoisationZipper.CacheDown[Char] = MemoisationZipper.emptyDown(RegexContextCharHashable)
+
+  val possibleEmailChars = "abcdedfghijklmnopqrstuvwxyz."
+  val emailRegex = possibleEmailChars.anyOf.+ ~ "@".r ~ possibleEmailChars.anyOf.+ ~ ".".r ~ possibleEmailChars.anyOf.+
+
+  val email_Accepting_strings: Map[Int, StainlessList[Char]] = string_sizes.map(n => (n, random_email_strings(n).toStainless)).toMap
+  def random_a_or_b(): String = {
+    if (r.nextBoolean()) "a" else "b"
+  }
+
+  def random_email_char(): String = {
+    val index = r.nextInt(possibleEmailChars.length)
+    possibleEmailChars(index).toString
+  }
+
+  def random_email_strings(n: Int): String = {
+    val usableLength = n - 2
+    val firstPartSize: Int = r.between(1, usableLength - 2 + 1)
+    val secondPartSize: Int = r.between(1, usableLength - firstPartSize - 1 + 1)
+    val thirdPartSize: Int = usableLength - firstPartSize - secondPartSize
+    val res: String = (1.to(firstPartSize)).map(_ => random_email_char()).mkString + "@" + (1.to(secondPartSize)).map(_ => random_email_char()).mkString + "." + (1.to(thirdPartSize)).map(_ => random_email_char()).mkString
+    // println(s"n = $n, usable length = $usableLength, first part size = $firstPartSize, second part size = $secondPartSize, res = $res")
+    assert(res.size == n)
+    res
+  }
+}
--- lexers/regex/verifiedlexer/src/main/scala/ch/epfl/benchmark/Utils.scala
+++ lexers/regex/verifiedlexer/src/main/scala/ch/epfl/benchmark/Utils.scala
@@ -11,6 +11,7 @@ import stainless.collection._
 
 import ch.epfl.map.Hashable
 import ch.epfl.lexer.VerifiedRegex._
+import ch.epfl.lexer.ZipperRegex._
 
 
 object RegexUtils {
@@ -24,6 +25,20 @@ object RegexUtils {
   extension (s: String) def * : Regex[Char] = r(s).*
   extension (s: String) def anyOf: Regex[Char] = s.toCharArray().toList.foldRight[Regex[Char]](EmptyLang())((c, acc) => Union(ElementMatch(c), acc))
   extension (s: String) def toStainless: stainless.collection.List[Char] = toStainlessList(s.toCharArray().toList)
+  extension (r: Regex[Char]) def asString(): String = r match {
+    case EmptyLang() => "∅"
+    case EmptyExpr() => "ε"
+    case ElementMatch(c) => c.toString
+    case Union(r1, r2) => s"(${r1.asString()} | ${r2.asString()})"
+    case Concat(r1, r2) => s"${r1.asString()}${r2.asString()}"
+    case Star(r1) => s"${r1.asString()}*"
+  }
+  extension [A] (l: stainless.collection.List[A]) def mkString(inter: String) : String = l match {
+    case stainless.collection.Nil() => ""
+    case stainless.collection.Cons(h, t) => h.toString + (if t.isEmpty then "" else inter + t.mkString(inter))
+  }
+  extension (c: Context[Char]) def asStringContext(): String = s"Sequence(${c.exprs.map(regex => regex.asString()).mkString(", ")})"
+  extension (z: Zipper[Char]) def asStringZipper(): String = s"Set(${z.map(c => c.asStringContext()).mkString(", ")})"
 
 
   def toStainlessList(l: scala.collection.immutable.List[Char]): stainless.collection.List[Char] = l match {
