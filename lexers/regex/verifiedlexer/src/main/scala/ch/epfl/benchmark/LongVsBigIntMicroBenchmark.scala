8b7faf1fbabf81e4a22caf99fad92a9c3919428a
Samuel Chassot <samuel.chassot@epfl.ch>
2025-08-28 15:01:17 +0000

    long bigint comparisons

--- lexers/regex/verifiedlexer/src/main/scala/ch/epfl/benchmark/LongVsBigIntMicroBenchmark.scala
+++ lexers/regex/verifiedlexer/src/main/scala/ch/epfl/benchmark/LongVsBigIntMicroBenchmark.scala
@@ -1,18 +1,42 @@
-package ch.epfl.lexer
-package benchmark
+package benchmark.lexer
 
 import java.util.concurrent.TimeUnit
+import org.openjdk.jmh.annotations.*
+import scala.util.Random
+import scala.compiletime.uninitialized
 
 @State(Scope.Benchmark)
 class LongVsBigIntMicroBenchmark {
+  
+  @Param(
+    Array(
+    "1", 
+    "2", 
+    "4", 
+    "8", 
+    "16", 
+    "32", 
+    "64", 
+    "128", 
+    "256", 
+    "512", 
+    "1024", 
+    "2048", 
+    "4096", 
+    "8192", 
+    "16384"
+    )
+  )
+  var size: String = uninitialized
 
   @Benchmark
   @BenchmarkMode(Array(Mode.AverageTime))
   @OutputTimeUnit(TimeUnit.MICROSECONDS)
   def compareIds_longs(): Unit = {
-    LongVsBigIntMicroBenchmarkUtils.randomLongs.foreach { id =>
-      LongVsBigIntMicroBenchmarkUtils.randomLongs.foreach { otherId =>
-        val _ = id == otherId
+    LongVsBigIntMicroBenchmarkUtils.randomLongs(size.toInt).foreach { id =>
+      LongVsBigIntMicroBenchmarkUtils.randomLongs(size.toInt).foreach { otherId =>
+        val out = id == otherId
+        assert(out == out)
       }
     }
   }
@@ -21,9 +45,10 @@ class LongVsBigIntMicroBenchmark {
   @BenchmarkMode(Array(Mode.AverageTime))
   @OutputTimeUnit(TimeUnit.MICROSECONDS)
   def compareIds_bigints(): Unit = {
-    LongVsBigIntMicroBenchmarkUtils.randomBigInts.foreach { id =>
-      LongVsBigIntMicroBenchmarkUtils.randomBigInts.foreach { otherId =>
-        val _ = id == otherId
+    LongVsBigIntMicroBenchmarkUtils.randomBigInts(size.toInt).foreach { id =>
+      LongVsBigIntMicroBenchmarkUtils.randomBigInts(size.toInt).foreach { otherId =>
+        val out = id == otherId
+        assert(out == out)
       }
     }
   }
@@ -34,6 +59,8 @@ object LongVsBigIntMicroBenchmarkUtils {
   val seed = 0x0ddba11
   val r = new Random(seed)
 
-  val randomLongs = Seq.fill(100)(r.nextLong())
-  val randomBigInts = randomLongs.map(BigInt(_))
+  val sizes = List(1, 2, 4, 8, 16, 32, 64, 128, 256, 512, 1024, 2048, 4096, 8192, 16384)
+
+  val randomLongs: Map[Int, Seq[Long]] = sizes.map(size => size -> Seq.fill(size)(r.nextLong())).toMap
+  val randomBigInts: Map[Int, Seq[BigInt]] = randomLongs.mapValues(_.map(BigInt(_))).toMap
 }
