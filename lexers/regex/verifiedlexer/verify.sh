85928f5132e1c71e61b82eb2fb78b0dd9c6edf8c
Samuel Chassot <samuel.chassot@epfl.ch>
2025-09-18 15:21:16 +0200

    add json manipulation to verifyu

--- lexers/regex/verifiedlexer/verify.sh
+++ lexers/regex/verifiedlexer/verify.sh
@@ -8,8 +8,9 @@ stainless-dotty\
  src/main/scala/ch/epfl/benchmark/Utils.scala\
  src/main/scala/ch/epfl/example/PythonLexer.scala\
  src/main/scala/ch/epfl/example/JSONLexer.scala\
+ src/main/scala/ch/epfl/example/JsonManipulation.scala\
  src/main/scala/ch/epfl/lexer/Vector.scala\
  --config-file=stainless.conf\
- -D-parallel=12 --functions=MemoisationRegex._,MemoisationRegex._,VerifiedRegex_,ZipperRegex._,VerifiedRegexMatcher._,VerifiedLexer._,Utils._,ListUtils._,SetUtils._,Types._,Vector._,ExampleAmyLexer._,ExamplePythonLexer._,ExampleJsonLexer._\
+ -D-parallel=12 --functions=MemoisationRegex._,MemoisationRegex._,VerifiedRegex_,ZipperRegex._,VerifiedRegexMatcher._,VerifiedLexer._,Utils._,ListUtils._,SetUtils._,Types._,Vector._,ExampleAmyLexer._,ExamplePythonLexer._,ExampleJsonLexer._,JsonManipulationExample._\
  $1
 
