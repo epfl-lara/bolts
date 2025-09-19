528d3779d21c14e80ddf771d47370d56b9c9b9c0
Samuel Chassot <samuel.chassot@epfl.ch>
2025-09-19 10:54:37 +0200

    new json manipulation example with rules preservation

--- lexers/regex/verifiedlexer/src/main/scala/ch/epfl/example/JsonManipulation.scala
+++ lexers/regex/verifiedlexer/src/main/scala/ch/epfl/example/JsonManipulation.scala
@@ -1,6 +1,7 @@
 package ch.epfl.lexer.example
 
 import stainless.annotation.extern
+import stainless.annotation.induct
 import stainless.annotation.pure
 import stainless.lang.Option
 import stainless.lang.Some
@@ -8,6 +9,7 @@ import stainless.lang.None
 import stainless.lang.decreases
 import stainless.lang.ghost as ghostExpr
 import stainless.lang.unfold
+import stainless.proof.check
 
 import stainless.collection.List
 import stainless.collection.Cons
@@ -28,6 +30,7 @@ import ch.epfl.lexer.VerifiedLexer.PrintableTokensFromTokens
 import ch.epfl.lexer.VerifiedLexer.emptyPrintableTokens
 import ch.epfl.lexer.ListUtils
 import stainless.collection.ListSpecs
+import scala.caps.use
 object JsonManipulationExample:
 
   /**
@@ -53,15 +56,20 @@ object JsonManipulationExample:
       acc
     else 
       val t = ts(from)
-      ghostExpr(assert(acc.forall(i => 0 <= i && i < size)))
+      // ghostExpr(assert(acc.forall(i => 0 <= i && i < size)))
       if isOpeningBrace(t, KeywordValue.LeftBrace) then
-        ghostExpr(assert(acc.forall(i => 0 <= i && i < size)))
-        ghostExpr(ListUtils.lemmaConcatPreservesForall(acc.list, List(from), (i: BigInt) => 0 <= i && i < size))
-        ghostExpr(assert((acc ++ Vector.fromList(List(from))).forall(i => 0 <= i && i < size)))
+        ghostExpr({
+          assert(acc.forall(i => 0 <= i && i < size))
+          ListUtils.lemmaConcatPreservesForall(acc.list, List(from), (i: BigInt) => 0 <= i && i < size)
+          assert((acc ++ Vector.fromList(List(from))).forall(i => 0 <= i && i < size))
+        })
         indicesOfOpenBraces(ts, size, from + 1, acc :+ from)
       else
         indicesOfOpenBraces(ts, size, from + 1, acc)
   }.ensuring(res => res.forall(i => 0 <= i && i < size))
+
+
+  def usesJsonRules(pt: PrintableTokens[Char]): Boolean = pt.rules == JsonLexer.rules
 
   /**
     * Lex the input as a Json content
@@ -73,24 +81,29 @@ object JsonManipulationExample:
     * @param cacheDown
     * @return
     */
-  def lexAndCheckPrintable(input: Vector[Char])(using cacheUp: MemoisationZipper.CacheUp[Char], cacheDown: MemoisationZipper.CacheDown[Char]): Option[PrintableTokens[Char]] = 
+  def lexAndCheckPrintable(input: Vector[Char])(using cacheUp: MemoisationZipper.CacheUp[Char], cacheDown: MemoisationZipper.CacheDown[Char]): Option[PrintableTokens[Char]] = {
     if JsonLexer.rules.isEmpty || !Lexer.rulesInvariant(JsonLexer.rules) then 
       None()
     else 
-      ghostExpr(cacheUp.lemmaInvariant())
-      ghostExpr(cacheDown.lemmaInvariant())
-      ghostExpr(assert(Lexer.lexThenRulesProduceEachTokenIndividually(JsonLexer.rules, input.list)))
-      ghostExpr(assert(!JsonLexer.rules.isEmpty))
-      ghostExpr(assert(Lexer.rulesInvariant(JsonLexer.rules)))
-      ghostExpr(assert(cacheUp.valid))
-      ghostExpr(assert(cacheDown.valid))
+      ghostExpr({
+        cacheUp.lemmaInvariant()
+        cacheDown.lemmaInvariant()
+        assert(Lexer.lexThenRulesProduceEachTokenIndividually(JsonLexer.rules, input.list))
+        assert(!JsonLexer.rules.isEmpty)
+        assert(Lexer.rulesInvariant(JsonLexer.rules))
+        assert(cacheUp.valid)
+        assert(cacheDown.valid)
+      })
       val (tokens, _) = Lexer.lexMem(JsonLexer.rules, input)
       PrintableTokensFromTokens(JsonLexer.rules, tokens)
+  }.ensuring(res => res.isEmpty || usesJsonRules(res.get))
 
 
   def slicesMulti(pt: PrintableTokens[Char], size: BigInt, indices: Vector[BigInt], acc: Vector[PrintableTokens[Char]] = Vector.empty[PrintableTokens[Char]]): Vector[PrintableTokens[Char]] = {
     require(pt.size == size)
     require(indices.forall(i => 0 <= i && i < size))
+    require(usesJsonRules(pt))
+    require(acc.forall(usesJsonRules))
     decreases(indices.size)
     if indices.size >= 2 then
       val from = indices(0)
@@ -100,15 +113,25 @@ object JsonManipulationExample:
       else 
         // Should never happen but we do not prove it
         pt.slice(to, from)
+      ghostExpr({
+        assert(usesJsonRules(slice))
+        ListUtils.lemmaConcatPreservesForall(acc.list, List(slice), usesJsonRules)
+        assert((acc :+ slice).forall(usesJsonRules))
+      })
       slicesMulti(pt, size, indices.tail, acc :+ slice)
     else if indices.size == 1 then
       val from = indices(0)
       val to = pt.size
       val slice = pt.slice(from, to)
+      ghostExpr({
+        assert(usesJsonRules(slice))
+        ListUtils.lemmaConcatPreservesForall(acc.list, List(slice), usesJsonRules)
+        assert((acc :+ slice).forall(usesJsonRules))
+      })
       slicesMulti(pt, size, indices.tail, acc :+ slice)
     else
       acc
-  }
+  }.ensuring(res => res.forall(usesJsonRules))
 
   /**
     * Parses the ID from a JSON object.
@@ -143,6 +166,7 @@ object JsonManipulationExample:
 
   def sortObjectsByID(objs: Vector[(Int, PrintableTokens[Char])]): Vector[(Int, PrintableTokens[Char])] = {
     decreases(objs.size)
+    require(objs.forall(t => usesJsonRules(t._2)))
     if objs.size <= 1 then objs
     else 
       val pivot = objs(objs.size / 2)
@@ -173,21 +197,30 @@ object JsonManipulationExample:
         }
         ListUtils.lemmaNotForallFilterShorter(objs.list, (t: (Int, PrintableTokens[Char])) => t._1 > pivot._1)
         assert(right.size < objs.size)
+
+        ListSpecs.filterSubseq(objs.list, (t: (Int, PrintableTokens[Char])) => t._1 < pivot._1)
+        ListSpecs.filterSubseq(objs.list, (t: (Int, PrintableTokens[Char])) => t._1 == pivot._1)
+        ListSpecs.filterSubseq(objs.list, (t: (Int, PrintableTokens[Char])) => t._1 > pivot._1)
+        ListUtils.lemmaForallSubseq(objs.filter(t => t._1 < pivot._1).list, objs.list, (t: (Int, PrintableTokens[Char])) => usesJsonRules(t._2))
+        ListUtils.lemmaForallSubseq(objs.filter(t => t._1 == pivot._1).list, objs.list, (t: (Int, PrintableTokens[Char])) => usesJsonRules(t._2))
+        ListUtils.lemmaForallSubseq(objs.filter(t => t._1 > pivot._1).list, objs.list, (t: (Int, PrintableTokens[Char])) => usesJsonRules(t._2))
+        ListUtils.lemmaConcatPreservesForall(sortObjectsByID(left).list, middle.list, (t: (Int, PrintableTokens[Char])) => usesJsonRules(t._2))
+        ListUtils.lemmaConcatPreservesForall(sortObjectsByID(left).list ++ middle.list, sortObjectsByID(right).list, (t: (Int, PrintableTokens[Char])) => usesJsonRules(t._2))
       })
       sortObjectsByID(left) ++ middle ++ sortObjectsByID(right) 
-  }
+  }.ensuring(res => res.forall(t => usesJsonRules(t._2)))
 
   def recombineSlices(objs: Vector[PrintableTokens[Char]], acc: PrintableTokens[Char]): Option[PrintableTokens[Char]] = {
+    require(objs.forall(usesJsonRules))
+    require(usesJsonRules(acc))
     decreases(objs.size)
     if objs.isEmpty then Some(acc)
     else 
-      if objs.head.rules != acc.rules then None()
-      else
-        val newAccOpt = objs.head.append(acc)
-        newAccOpt match
-          case Some(newAcc) => recombineSlices(objs.tail, newAcc)
-          case None() => None()
-  }
+      val newAccOpt = objs.head.append(acc)
+      newAccOpt match
+        case Some(newAcc) => recombineSlices(objs.tail, newAcc)
+        case None() => None()
+  }.ensuring(res => res.isEmpty || usesJsonRules(res.get))
 
   def main(path: String)(using cacheUp: MemoisationZipper.CacheUp[Char], cacheDown: MemoisationZipper.CacheDown[Char]): Option[Vector[Char]] = {
     val input: Vector[Char] = openFile(path)
@@ -200,10 +233,28 @@ object JsonManipulationExample:
         val slices: Vector[PrintableTokens[Char]] = slicesMulti(printableTokens, tokensSize, 0 +: indices)
         // Now we have slices of PrintableTokens, all separable on their own, as provided by the specification of slice
 
-        val slicesWithIds = slices.map(obj => (parseID(obj), obj))
+        def addId(pt: PrintableTokens[Char]): (Int, PrintableTokens[Char]) = (parseID(pt), pt)
+        ghostExpr({
+          def lemmaAddIdsPreservesRules(@induct s: List[PrintableTokens[Char]]): Unit = {
+            require(s.forall(usesJsonRules))
+          }.ensuring(_ => s.forall(pt => usesJsonRules(addId(pt)._2)))
+          lemmaAddIdsPreservesRules(slices.list)
+          ListSpecs.mapPred(slices.list, addId, (t: (Int, PrintableTokens[Char])) => usesJsonRules(t._2))
+        })
+        val slicesWithIds = slices.map(addId)
         val orderedSlices = sortObjectsByID(slicesWithIds)
+        
+        def removeId(t: (Int, PrintableTokens[Char])): PrintableTokens[Char] = t._2
+        ghostExpr({
+          def lemmaRemoveIdsPreservesRules(@induct l: List[(Int, PrintableTokens[Char])]): Unit = {
+            require(l.forall(p => usesJsonRules(p._2)))
+          }.ensuring(_ => l.forall(p => usesJsonRules(removeId(p))))
+          lemmaRemoveIdsPreservesRules(orderedSlices.list)
+          ListSpecs.mapPred(orderedSlices.list, removeId, (pt: PrintableTokens[Char]) => usesJsonRules(pt))
+          check(orderedSlices.map(removeId).forall(pt => usesJsonRules(pt)))
+        })
         // Now that we have ordered our objects, we recombine them
-        val recombined: Option[PrintableTokens[Char]] = recombineSlices(orderedSlices.map(_._2), emptyPrintableTokens(JsonLexer.rules))
+        val recombined: Option[PrintableTokens[Char]] = recombineSlices(orderedSlices.map(removeId), emptyPrintableTokens(JsonLexer.rules))
         recombined match
           case Some(newTokens) => {
             // Now we have a new PrintableTokens, which checked only the separable predicate at the boundaries of slices
