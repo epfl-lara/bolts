fefe98178a58d15bc542db45f6ad5ad969de5e3f
Samuel Chassot <samuel.chassot@epfl.ch>
2025-09-26 05:15:14 +0000

    script

--- lexers/regex/verifiedlexer/run_benchmarks.sh
+++ lexers/regex/verifiedlexer/run_benchmarks.sh
@@ -1,7 +1,7 @@
 # Create a directory named results_{current date with time} if it doesn't exist
 DIRECTORY_NAME="results_$(date +'%d.%m.%Y')"
 mkdir -p "$DIRECTORY_NAME"
-sbt "Jmh/run -i 5 -wi 5 -f1 -t1 ch.epfl.lexer.benchmark.LexerRegexBenchmark" > "$DIRECTORY_NAME/json_lexerregex_benchmark_wi_5_i_5_laraserver4.txt"
-sbt "Jmh/run -i 5 -wi 5 -f1 -t1 ch.epfl.lexer.benchmark.RegexBenchmark" > "$DIRECTORY_NAME/json_regex_benchmark_wi_5_i_5_laraserver4.txt"
+sbt "Jmh/run -i 5 -wi 5 -f1 -t1 ch.epfl.lexer.benchmark.LexerRegexBenchmark" > "$DIRECTORY_NAME/lexerregex_benchmark_wi_5_i_5_laraserver4.txt"
+sbt "Jmh/run -i 5 -wi 5 -f1 -t1 ch.epfl.lexer.benchmark.RegexBenchmark" > "$DIRECTORY_NAME/regex_benchmark_wi_5_i_5_laraserver4.txt"
 sbt "Jmh/run -i 5 -wi 5 -f1 -t1 ch.epfl.lexer.benchmark.lexer.JsonLexerBenchmark" > "$DIRECTORY_NAME/json_lexer_benchmark_wi_5_i_5_laraserver4.txt"
 sbt "Jmh/run -i 5 -wi 5 -f1 -t1 ch.epfl.lexer.benchmark.lexer.JsonManipulationBenchmark" > "$DIRECTORY_NAME/json_manipulation_benchmark_wi_5_i_5_laraserver4.txt"
