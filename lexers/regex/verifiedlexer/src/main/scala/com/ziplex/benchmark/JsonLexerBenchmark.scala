99dcce71c723ad4b275d63f777ea031cb0e94a21
Samuel Chassot <samuel.chassot@epfl.ch>
2026-01-06 09:54:12 +0100

    new versions with memoization only for derivatives for V2

--- lexers/regex/verifiedlexer/src/main/scala/com/ziplex/benchmark/JsonLexerBenchmark.scala
+++ lexers/regex/verifiedlexer/src/main/scala/com/ziplex/benchmark/JsonLexerBenchmark.scala
@@ -82,6 +82,36 @@ class JsonLexerBenchmark {
   @Benchmark
   @BenchmarkMode(Array(Mode.AverageTime))
   @OutputTimeUnit(TimeUnit.MICROSECONDS)
+  def lex_ZipperV1NonMem(bh: Blackhole): Unit = {
+    val (tokens, suffix) = Lexer.lexV1(JsonLexer.rules, JsonLexerBenchmarkUtils.exampleFileContents(file))
+    bh.consume(suffix.isEmpty)
+    // assert(suffix.isEmpty)
+  }
+
+  @Benchmark
+  @BenchmarkMode(Array(Mode.AverageTime))
+  @OutputTimeUnit(TimeUnit.MICROSECONDS)
+  def lex_ZipperV2NonMem(bh: Blackhole): Unit = {
+    val (tokens, suffix) = Lexer.lex(JsonLexer.rules, JsonLexerBenchmarkUtils.exampleFileContents(file))
+    bh.consume(suffix.isEmpty)
+    // assert(suffix.isEmpty)
+  }
+
+  @Benchmark
+  @BenchmarkMode(Array(Mode.AverageTime))
+  @OutputTimeUnit(TimeUnit.MICROSECONDS)
+  def lex_ZipperV2MemOnlyDeriv(bh: Blackhole): Unit = {
+    val (tokens, suffix) = Lexer.lexV2MemOnlyDeriv(JsonLexer.rules, JsonLexerBenchmarkUtils.exampleFileContents(file))(
+      using ClassTag.Char,
+      JsonLexerBenchmarkUtils.zipperCacheUp,
+      JsonLexerBenchmarkUtils.zipperCacheDown)
+    bh.consume(suffix.isEmpty)
+    // assert(suffix.isEmpty)
+  }
+
+  @Benchmark
+  @BenchmarkMode(Array(Mode.AverageTime))
+  @OutputTimeUnit(TimeUnit.MICROSECONDS)
   def separability_pred(bh: Blackhole): Unit = {
     val tokens = JsonLexerBenchmarkUtils.exampleFileTokens(file)
     val separable =  Lexer.separableTokensMem(tokens, JsonLexer.rules)(
