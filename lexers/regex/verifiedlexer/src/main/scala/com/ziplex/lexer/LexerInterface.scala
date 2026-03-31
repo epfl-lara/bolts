4144e346dbe69412100735d3f4e8987303262734
Samuel Chassot <samuel.chassot@epfl.ch>
2026-03-31 07:19:40 +0200

    update vector's import in the lexer, in case someone wants to use it

--- lexers/regex/verifiedlexer/src/main/scala/com/ziplex/lexer/LexerInterface.scala
+++ lexers/regex/verifiedlexer/src/main/scala/com/ziplex/lexer/LexerInterface.scala
@@ -44,10 +44,10 @@ import stainless.lang.{ghost => ghostExpr, _}
 // BalanceConc is a sequence implementation based on a binary tree, fully verified although slower
 
 // type Sequence[T] = Vector[T]
-// inline def emptySeq[T]: Sequence[T] = Vector.empty
-// inline def singletonSeq[T](t: T): Sequence[T] = Vector.singleton(t)
-// inline def seqFromList[T](l: List[T]): Sequence[T] = Vector.fromList(l)
-// inline def seqFromArray[T: ClassTag](arr: IArray[T]): Sequence[T] = Vector.fromArray(arr)
+// def emptySeq[T](): Sequence[T] = Vector.empty
+// def singletonSeq[T](t: T): Sequence[T] = Vector.singleton(t)
+// def seqFromList[T](l: List[T]): Sequence[T] = Vector.fromList(l)
+// def seqFromArray[T: ClassTag](arr: IArray[T]): Sequence[T] = Vector.fromArray(arr)
 type Sequence[T] = BalanceConc[T]
 def emptySeq[T: ClassTag](): Sequence[T] = BalanceConcObj.empty[T]
 def singletonSeq[T: ClassTag](t: T): Sequence[T] = BalanceConcObj.singleton(t)
